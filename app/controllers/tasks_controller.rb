f1b6a3ca5a1775898cfd4592b453fae1ad647a03
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-12-03 15:54:57 -0600

    correcting typos and creating the update route, misc cleanup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,17 +11,31 @@ class TasksController < ApplicationController
     erb :'tasks/newtask'
   end
 
-  post '/newtask' do   
+  get '/tasks/:id/edit' do
     redirect_if_not_logged_in
-      Task.create(params)
-      redirect "/tasks"
-    end
+    @task = Task.find_by_id(params[:id])
+    erb :'tasks/edit'
+  end
 
+  patch '/tasks/:id' do
+    redirect_if_not_logged_in
+    @task = Task.find_by_id(params[:id])
+    @task.update(date_worked: params[:date_worked], task: params[:task], project_title: params[:project_title], hrs_worked: params[:hrs_worked])
+    @task.save
+    redirect to '/tasks'
+  end
 
   get '/tasks/:id' do
     redirect_if_not_logged_in
       @task = Task.find_by_id(params[:id])
       erb :'tasks/show'
   end
+
+  post '/newtask' do   
+    redirect_if_not_logged_in
+    Task.create(params)
+    redirect to '/tasks'
+  end
+
   
 end
