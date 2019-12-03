7a568e7c9e00d68fda27aed1b3e0e8fca019a5b5
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-12-03 08:54:44 -0600

    created form for signup, created signup routes in UserController, corrected routes in layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !current_user
+      erb :'users/signup'
+    else
+      redirect to '/tasks'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(:name => params[:username], :password_digest => params[:password])
+      session[:user_id] = @user.id 
+      redirect '/tasks'
+    end
+  end
+
 end
