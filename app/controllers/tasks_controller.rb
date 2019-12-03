b9104a7eed90b00e254825cd0b2e978004d0cb44
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-12-03 12:53:36 -0600

    create route and form for new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,27 @@ class TasksController < ApplicationController
     erb :'tasks/index'
   end
 
+  get '/newtask' do
+    redirect_if_not_logged_in
+    erb :'tasks/newtask'
+  end
+
+  post '/newtask' do
+    if logged_in?
+      if params[:task] = ""
+        redirect to '/newtask'
+      else
+        @task = current_user.tasks.build(date_worked: params[:date_worked], task: params[:task], project_title: params[:project_title], hrs_worked: params[:hrs_worked])
+        if @task.save
+          redirect to '/tasks/#{@task.id}'
+        else
+          redirect to '/newtask'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  
+  end
+
 end
